1b4d242f4897be3ed3fdabfcb0072548a856e05f
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-06-22 06:53:37 +0000

    add specifc order

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
+
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.environ['POSTGRES_HOST'],
@@ -18,28 +19,62 @@ def get_db_connection():
 
 
 @app.route('/')
-def index():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM orders")
-    results = cursor.fetchall()
-    formatted_results = []
-    for row in results:
-        order = {
-            "order_id": row[0],
-            "customer": {
-                "name": row[1],
-                "email": row[2],
-                "address": row[3]
-            },
-            "product_name": row[4],
-            "quantity": row[5],
-            "order_date": row[6],
-            "priority": row[7]
-        }
-        formatted_results.append(order)
+def all():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM orders")
+        results = cursor.fetchall()
+        formatted_results = []
+        for row in results:
+            order = {
+                "order_id": row[0],
+                "customer": {
+                    "name": row[1],
+                    "email": row[2],
+                    "address": row[3]
+                },
+                "product_name": row[4],
+                "quantity": row[5],
+                "order_date": row[6],
+                "priority": row[7]
+            }
+            formatted_results.append(order)
 
-    return jsonify(formatted_results)
+        return jsonify(formatted_results)
+    except psycopg2.Error:
+        return jsonify({'error': 'Failed to retrieve order'}), 500
+
+
+@app.route('/orders/<order_id>', methods=['GET'])
+def get_order_by_number(order_id):
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Retrieve the order from the database
+        cursor.execute("SELECT * FROM orders WHERE order_id = %s", (order_id,))
+        order = cursor.fetchone()
+
+        if order:
+            # Format the order as a dictionary
+            formatted_order = {
+                "order_id": order[0],
+                "customer": {
+                    "name": order[1],
+                    "email": order[2],
+                    "address": order[3]
+                },
+                "product_name": order[4],
+                "quantity": order[5],
+                "order_date": order[6],
+                "priority": order[7]
+            }
+            return jsonify(formatted_order)
+        else:
+            return jsonify({'error': 'Order not found'}), 404
+    except psycopg2.Error:
+        return jsonify({'error': 'Failed to retrieve order'}), 500
 
 
 if __name__ == '__main__':
