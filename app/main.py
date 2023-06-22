644a9b62a3a838410ae27ab541e6f68d0b488a3b
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2023-06-22 11:33:38 +0000

    add create_order

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 import os
 
@@ -17,7 +17,9 @@ def get_db_connection():
     # conn.close()
     return conn
 
-# Get all orders 
+# Get all orders
+
+
 @app.route('/')
 def all():
     try:
@@ -46,7 +48,9 @@ def all():
         return jsonify({'error': 'Failed to retrieve order'}), 500
 
 # Get order details by order_id
-@app.route('/orders/<order_id>', methods=['GET'])
+
+
+@app.route('/order/<int:order_id>', methods=['GET'])
 def get_order_by_number(order_id):
     try:
         conn = get_db_connection()
@@ -76,8 +80,10 @@ def get_order_by_number(order_id):
     except psycopg2.Error:
         return jsonify({'error': 'Failed to retrieve order'}), 500
 
+
 # Delete an order by order_id
-@app.route('/order/<order_id>', methods=['DELETE'])
+
+@app.route('/order/<int:order_id>', methods=['DELETE'])
 def delete_order(order_id):
     try:
         conn = get_db_connection()
@@ -94,6 +100,35 @@ def delete_order(order_id):
             return jsonify({'error': 'Order not found'}), 404
     except psycopg2.Error:
         return jsonify({'error': 'Failed to delete order'}), 500
+
+# Create a new order
+
+
+@app.route('/order', methods=['POST'])
+def create_order():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        # Extract order details from the JSON payload
+        order_details = request.json
+        order_id = order_details['order_id']
+        customer = order_details['customer']
+        product_name = order_details['product_name']
+        quantity = order_details['quantity']
+        order_date = order_details['order_date']
+        priority = order_details['priority']
+
+        # Insert the order into the database
+        cursor.execute("INSERT INTO orders (order_id, customer_name, customer_email, customer_address, product_name, quantity, order_date, priority) VALUES (%s,%s, %s, %s, %s, %s, %s, %s) RETURNING order_id",
+                       (order_id, customer['name'], customer['email'], customer['address'], product_name, quantity, order_date, priority))
+        conn.commit()
+
+        return jsonify({'order_id': order_id}), 201
+    except KeyError:
+        return jsonify({'error': 'Invalid request payload'}), 400
+    except psycopg2.Error:
+        return jsonify({'error': 'Failed to create order'}), 500
 
 
 if __name__ == '__main__':
